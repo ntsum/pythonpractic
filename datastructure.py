27bed23ab965f0067ab0ddb2d59a04f72209fb50
ntsum <natsukajiwaram@gmail.com>
2020-01-06 14:44:43 -0500

    python practice

---
+++ datastructure.py
@@ -0,0 +1,41 @@
+a = 10
+a = (a+a)
+print(a)
+print(type(a))
+a = 29.765
+print(type(a))
+
+my_income = 100
+
+tax_rate = 0.1
+
+my_taxes = my_income * tax_rate
+print(my_taxes)
+
+# hashtags are comments
+mystring = "Hello World"
+print(mystring)
+print(mystring[0])
+print(mystring[8])
+print(mystring[-4])
+
+# slicing
+mystring = 'abcdefghijk'
+print(mystring)
+mystring[:3]
+print(mystring[3:7])
+
+name = "Sam"
+print(name)
+last_letters = name[1:]
+print(last_letters)
+Sam = 'P' + last_letters
+print(Sam)
+
+sleep = 'z'
+print(sleep * 20)
+
+y = 'Hello World'
+print(y.upper())
+print(y.lower())
+print(y.split('l'))
